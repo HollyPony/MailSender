b82ea67d111c23185efb07a515fc9c7859678232
Arthur Agombart <aagombart@gmail.com>
2015-07-09 13:58:11 +0200

    Create a property class Manager

--- src/main/java/io/liomka/mailsender/SendMailService.java
+++ src/main/java/io/liomka/mailsender/SendMailService.java
@@ -1,5 +1,6 @@
 package io.liomka.mailsender;
 
+import io.liomka.mailsender.utils.UserProperties;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
@@ -8,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
-import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
@@ -19,7 +19,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
 import java.util.Collection;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -33,80 +32,35 @@ public class SendMailService {
 
     private final static Logger LOG = LoggerFactory.getLogger(SendMailService.class);
 
-    // Args const
-    private final static String MAIL_TO = "MAIL_TO";
-    private final static String MAIL_CC = "MAIL_CC";
-    private final static String MAIL_FROM = "MAIL_FROM";
-    private final static String MAIL_SMTP_PORT = "MAIL_SMTP_PORT";
-    private final static String MAIL_SMTP_PORT_DEFAULT = "25";
-    private final static String MAIL_ADRESSE_HOST = "MAIL_ADRESSE_HOST";
-    private final static String MAIL_ADRESSE_HOST_DEFAULT = "localhost";
-
-    // Default properties consts
-    private final static String MAIL_PATH_DEFAULT = "./Mails/";
-
-    // Properties file path
-    private final static String propertiesPath = "mail.properties";
-
-    class Contexte {
-        // Properties
-        public Date date;
-        public String from;
-        public String to;
-        public String cc;
-        public String port;
-        public String host;
-
-        public Transport transport = null;
-        public Session session = null;
-    }
-
     private void execute() {
+        final UserProperties usr = new UserProperties();
 
-        LOG.info("Read properties on ".concat(propertiesPath));
-        final Properties properties = loadProperties();
-
-        final String mailPath = properties.getProperty("MAIL_PATH", MAIL_PATH_DEFAULT);
+        usr.getDate();
+        final String mailPath = usr.getMailPath();
 
         LOG.info("Read files on \"".concat(mailPath).concat("\" !"));
         final Map<File, Message> mails = loadMails(mailPath);
 
-
-        final Contexte ctx = new Contexte();
-        ctx.date = new Date();
-        ctx.to = properties.getProperty(MAIL_TO, null);
-        ctx.cc = properties.getProperty(MAIL_CC, StringUtils.EMPTY);
-        ctx.port = properties.getProperty(MAIL_SMTP_PORT, MAIL_SMTP_PORT_DEFAULT);
-        ctx.host = properties.getProperty(MAIL_ADRESSE_HOST, MAIL_ADRESSE_HOST_DEFAULT);
-        ctx.from = properties.getProperty(MAIL_FROM, StringUtils.EMPTY);
-
+        Transport transport = null;
         try {
             // Il faut au moins des smtp host et port
-            if (StringUtils.isEmpty(ctx.host))
+            if (StringUtils.isEmpty(usr.getHost()))
                 throw new Exception("Cannot send mail, server address is missing");
 
-            if (StringUtils.isEmpty(ctx.port))
+            if (StringUtils.isEmpty(usr.getPort()))
                 throw new Exception("Cannot send mail: server port is missing");
 
-            // Properties
-            properties.setProperty("mail.transport.protocol", "smtp");
-            properties.setProperty("mail.smtp.port", ctx.port.trim());
-            properties.setProperty("mail.smtp.host", ctx.host.trim());
-            properties.setProperty("mail.smtp.connectiontimeout", "5000");
-            properties.setProperty("mail.smtp.timeout", "25000");
-            properties.setProperty("mail.smtp.auth", "false");
-
-            ctx.session = javax.mail.Session.getInstance(properties, null);
-
+            LOG.info("Get session ...");
+            usr.getSession();
             LOG.info("Get transport ...");
-            ctx.transport = ctx.session.getTransport();
+            transport = usr.getTransport();
             LOG.info("Connect on transport");
-            ctx.transport.connect();
+            transport.connect();
             LOG.info("Connected");
 
             LOG.info("Sending " + mails.values().size() + " mails");
             for (final Map.Entry<File, Message> mailEntry : mails.entrySet()) {
-                sendMail(ctx, mailEntry);
+                sendMail(usr, mailEntry);
             }
         } catch (NoSuchProviderException e) {
             e.printStackTrace();
@@ -115,9 +69,9 @@ public class SendMailService {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            if (ctx.transport != null) {
+            if (transport != null) {
                 try {
-                    ctx.transport.close();
+                    transport.close();
                 } catch (MessagingException e) {
                     e.printStackTrace();
                 }
@@ -125,7 +79,7 @@ public class SendMailService {
         }
     }
 
-    private void sendMail(final Contexte ctx, final Map.Entry<File, Message> mailEntry) {
+    private void sendMail(final UserProperties usr, final Map.Entry<File, Message> mailEntry) {
         InputStream mailStream = null;
 
         try {
@@ -142,18 +96,17 @@ public class SendMailService {
             final String fromDefault = System.getProperty("user.name").concat("@")
                     .concat(InetAddress.getLocalHost().getHostName());
 
-            final String from = StringUtils.defaultIfEmpty(ctx.from, StringUtils.defaultIfEmpty(fromEml, fromDefault));
+            final String from = StringUtils.defaultIfEmpty(usr.getFrom(), StringUtils.defaultIfEmpty(fromEml, fromDefault));
 
             // Le Message
-            MimeMessage message = new MimeMessage(ctx.session, mailStream);
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(ctx.to.trim()));
-            message.setRecipients(Message.RecipientType.CC, InternetAddress.parse(ctx.cc.trim()));
+            MimeMessage message = new MimeMessage(usr.getSession(), mailStream);
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(usr.getTo().trim()));
+            message.setRecipients(Message.RecipientType.CC, InternetAddress.parse(usr.getCc().trim()));
             message.setFrom(new InternetAddress(from.trim()));
-            message.setSentDate(ctx.date);
-
+            message.setSentDate(usr.getDate());
 
             // envoi du mail
-            ctx.transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
+            usr.getTransport().sendMessage(message, message.getRecipients(Message.RecipientType.TO));
             LOG.info("Mail sent");
         } catch (Exception e) {
             LOG.info("fail");
@@ -169,6 +122,11 @@ public class SendMailService {
         }
     }
 
+    /**
+     * Load all mails into HashMap Java compliant.
+     * @param mailPath
+     * @return
+     */
     private Map<File,Message> loadMails(String mailPath) {
         final Map<File, Message> result = new HashMap<File, Message>();
         final File mailDirectory = new File(mailPath);
@@ -194,30 +152,6 @@ public class SendMailService {
         }
 
         return result;
-    }
-
-    private Properties loadProperties() {
-        InputStream propertiesFile = null;
-        final Properties properties = new Properties();
-        try {
-            propertiesFile = new FileInputStream(propertiesPath);
-
-            LOG.info("Load properties on : " + propertiesPath);
-            properties.load(propertiesFile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (propertiesFile != null) {
-                try {
-                    propertiesFile.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return properties;
     }
 
     public static void main (String[] args) {
---
+++ src/main/java/io/liomka/mailsender/utils/UserProperties.java
@@ -0,0 +1,140 @@
+package io.liomka.mailsender.utils;
+
+import org.apache.commons.lang.StringUtils;
+
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.Properties;
+
+/**
+ * Created by aagombart on 09/07/2015.
+ */
+public class UserProperties {
+
+    // Properties file path
+    private final static String propertiesPath = "mail.properties";
+
+    // Args const
+    private final static String MAIL_TO = "MAIL_TO";
+    private final static String MAIL_CC = "MAIL_CC";
+    private final static String MAIL_FROM = "MAIL_FROM";
+    private final static String MAIL_SMTP_PORT = "MAIL_SMTP_PORT";
+    private final static String MAIL_SMTP_PORT_DEFAULT = "25";
+    private final static String MAIL_ADRESSE_HOST = "MAIL_ADRESSE_HOST";
+    private final static String MAIL_ADRESSE_HOST_DEFAULT = "localhost";
+    private final static String MAIL_PATH_DEFAULT = "./Mails/";
+
+    // Properties
+    private String mailPath = null;
+    private Date date = null;
+    private String from = null;
+    private String to = null;
+    private String cc = null;
+    private String port = null;
+    private String host = null;
+
+    private Transport transport = null;
+    private Session session = null;
+
+    private Properties properties = null;
+
+    public UserProperties() {
+        // Load properties file
+        properties = new Properties();
+        InputStream propertiesFile = null;
+        try {
+            propertiesFile = new FileInputStream(propertiesPath);
+            System.out.println(propertiesPath);
+            properties.load(propertiesFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (propertiesFile != null) {
+                try {
+                    propertiesFile.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // Initialize mail parameters
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.smtp.port", getPort().trim());
+        properties.setProperty("mail.smtp.host", getHost().trim());
+        properties.setProperty("mail.smtp.connectiontimeout", "5000");
+        properties.setProperty("mail.smtp.timeout", "25000");
+        properties.setProperty("mail.smtp.auth", "false");
+    }
+
+    public String getMailPath() {
+        if (mailPath == null) {
+            mailPath = properties.getProperty("MAIL_PATH", MAIL_PATH_DEFAULT);
+        }
+        return mailPath;
+    }
+
+    public Date getDate() {
+        if (date == null) {
+            date = new Date();
+        }
+        return date;
+    }
+
+    public String getTo() {
+        if (to == null) {
+            to = properties.getProperty(MAIL_TO, null);
+        }
+        return to;
+    }
+
+    public String getCc() {
+        if (cc == null) {
+            cc = properties.getProperty(MAIL_CC, StringUtils.EMPTY);
+        }
+        return cc;
+    }
+
+    public String getPort() {
+        if (port == null) {
+            port = properties.getProperty(MAIL_SMTP_PORT, MAIL_SMTP_PORT_DEFAULT);
+        }
+        return port;
+    }
+
+    public String getHost() {
+        if (host == null) {
+            host = properties.getProperty(MAIL_ADRESSE_HOST, MAIL_ADRESSE_HOST_DEFAULT);
+        }
+        return host;
+    }
+
+    public String getFrom() {
+        if (from == null) {
+            from = properties.getProperty(MAIL_FROM, StringUtils.EMPTY);
+        }
+        return from;
+    }
+
+    public Session getSession() {
+        if (session == null) {
+            session = javax.mail.Session.getInstance(properties, null);
+        }
+        return session;
+    }
+
+    public Transport getTransport() throws NoSuchProviderException {
+        if (transport == null) {
+            transport = getSession().getTransport();
+        }
+        return transport;
+    }
+}
